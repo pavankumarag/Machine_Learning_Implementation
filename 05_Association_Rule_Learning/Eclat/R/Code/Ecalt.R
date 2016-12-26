c8042b857c7335495a84342b399dfd9376335532
Pavan Kumar A G <pavan.govindraj@nutanix.com>
2016-12-26 12:19:00 +0530

    Eclat related stuff push

---
+++ 05_Association_Rule_Learning/Eclat/R/Code/Ecalt.R
@@ -0,0 +1,16 @@
+#Eclat
+
+#install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv',sep = ",", 
+                            rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+#Training the Eclat on the dataset
+rules = eclat(data = dataset, parameter = list(support = 0.004, minlen=2))
+#support calculated as 3*7/7500
+
+#Visualising the results
+inspect(sort(rules, by = 'support')[1:10])
